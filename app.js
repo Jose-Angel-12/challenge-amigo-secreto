1005e57f018b9ce28e7cd75f73ec854ff7983237
Jose-Angel-12 <jajaraba12@gmail.com>
2025-02-07 20:01:10 -0500

    actulizando function actualizarListaAmigos

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 let amigos = [];
 
 function agregarAmigo() {
-    let amigo = document.getElementById('amigo').value;
+    let amigo = document.getElementById('amigo').value.trim();
+
     if (amigo === '') {
         alert('Por favor, inserte un nombre.')
-    } else {
-        amigos.push(amigo);
-        
-        console.log(amigos);
+        return;
     }
+
+    amigos.push(amigo);
+    console.log(amigos);
     document.querySelector('#amigo').value = '';
     actualizarListaAmigos();
-    return;
 }
 
 function actualizarListaAmigos() {
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = '';
 
-for (let i = 0; i < amigos.length; i++){
-    let li = document.createElement('li');
-    li.textContent = amigos[i];
-    console.log(amigos[i]);
-    lista.appendChild(li);
-}
-
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        console.log(amigos[i]);
+        lista.appendChild(li);
+    }
     return;
 }
