cf3d764832329ad5da01aec7a10d88faed745504
Jose-Angel-12 <jajaraba12@gmail.com>
2025-02-07 19:47:55 -0500

    creando function actualizarLitaAmigos()

--- app.js
+++ app.js
@@ -6,7 +6,24 @@ function agregarAmigo() {
         alert('Por favor, inserte un nombre.')
     } else {
         amigos.push(amigo);
+        
+        console.log(amigos);
     }
     document.querySelector('#amigo').value = '';
+    actualizarListaAmigos();
+    return;
+}
+
+function actualizarListaAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+for (let i = 0; i < amigos.length; i++){
+    let li = document.createElement('li');
+    li.textContent = amigos[i];
+    console.log(amigos[i]);
+    lista.appendChild(li);
+}
+
     return;
 }
